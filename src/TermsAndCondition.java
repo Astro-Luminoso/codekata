fa22346644ce4ce71874d0aa3dc41ab4090ec3df
hanbyeol.yang <beuri97@gmail.com>
2026-04-06 12:06:59 +0900

    terms and conditions question - No71

--- src/TermsAndCondition.java
+++ src/TermsAndCondition.java
@@ -4,7 +4,7 @@ public class TermsAndCondition {
 
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answer = new ArrayList<>();
-        int[] todayDate = Arrays.stream(today.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        String[] todayDate = today.split("\\.");
         Map<String, Integer> termMap = new HashMap<>();
         for (String term : terms) {
             String[] parts = term.split(" ");
@@ -13,9 +13,10 @@ public class TermsAndCondition {
         for (int i = 0; i < privacies.length; i++) {
             String[] privacyAndTerm = privacies[i].split(" ");
             String[] privacyDate = privacyAndTerm[0].split("\\.");
-            int totalDays = (todayDate[0] - Integer.parseInt(privacyDate[0])) * 12 * 28 +
-                    (todayDate[1] - Integer.parseInt(privacyDate[1])) * 28 +
-                    (todayDate[2] - Integer.parseInt(privacyDate[2]));
+            int totalDays =
+                    (Integer.parseInt(todayDate[0]) - Integer.parseInt(privacyDate[0])) * 12 * 28 +
+                            (Integer.parseInt(todayDate[1]) - Integer.parseInt(privacyDate[1])) * 28 +
+                            (Integer.parseInt(todayDate[2]) - Integer.parseInt(privacyDate[2]));
             if (totalDays >= termMap.get(privacyAndTerm[1]) * 28) answer.add(i + 1);
         }
         return answer.stream().mapToInt(Integer::intValue).toArray();
