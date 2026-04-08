1ecc6ddcbffe4898f6d72b460a712548048e5c43
hanbyeol.yang <beuri97@gmail.com>
2026-04-08 09:38:37 +0900

    No8 Angles

--- src/Angles.java
+++ src/Angles.java
@@ -1,4 +1,9 @@
 public class Angles {
+    public int solution(int angle) {
 
-
+        return (angle < 90 && angle > 0) ? 1
+                : (angle == 90) ? 2
+                  : (angle < 180) ? 3
+                    : 4;
+    }
 }
